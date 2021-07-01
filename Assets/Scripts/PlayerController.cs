e1c0b7a95f8accddf2fd8c241a18ac2f608fc655
StacyIssac <68945421+StacyIssac@users.noreply.github.com>
2021-07-01 10:44:54 +0800

    Create PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("ÒÆ¶¯ÓëÐý×ª")]
+    public Transform cam;
+    public float moveSpeed = 5f;
+    public float turnSmoothTime = 0.1f;
+    public float rotateSpeed = 4f;
+    float turnSmoothVelocity = 0, angle = 0;
+
+    Vector3 moveDir;
+    Vector3 moveAmount;
+    Vector3 direction;
+    Rigidbody rb;
+
+    [Header("ÌøÔ¾")]
+    public float jumpSpeed = 1f;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask groundLayer;
+    private bool isGround, jumpPressed = false;
+    int jumpCount = 0;
+
+    [Header("Ð±ÆÂ¼ì²â")]
+    public float rayLength = 0.5f;
+    public float maxSlopeAngle = 45f;
+    Vector3 hitNormal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        ToHideCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(OnSlope())
+        //{
+        //    moveDir = Vector3.ProjectOnPlane(moveDir, hitNormal).normalized;
+        //}
+
+        PlayerMovement();
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        if(direction.magnitude >= 0.1f)
+        {
+            rb.MovePosition(rb.position + moveAmount);
+        }
+
+        if (isGround)
+        {
+            jumpCount = 1;
+        }
+        if(jumpPressed && isGround)
+        {
+            rb.velocity += new Vector3(0, jumpSpeed, 0);
+            jumpCount--;
+            jumpPressed = false;
+        }
+
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    }
+
+    private void PlayerMovement()
+    {
+        //Î»ÒÆ
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        moveAmount = moveDir.normalized * moveSpeed * Time.deltaTime;
+
+        //Ðý×ª
+        angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+    }
+
+    private void Jump()
+    {
+        //ÂäµØÅÐ¶Ï
+        isGround = Physics.CheckSphere(groundCheck.position, checkRadius, groundLayer);
+        //ÅÐ¶ÏÌøÔ¾
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPressed = true;
+        }
+    }
+
+    void ToHideCursor()
+    {
+        //Òþ²ØÊó±ê
+        Cursor.visible = false;
+    }
+
+    bool OnSlope()
+    {
+        //ÉäÏß¼ì²âÅÐ¶ÏÐ±ÆÂ
+        Ray ray = new Ray(groundCheck.position, Vector3.down);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, rayLength))
+        {
+            hitNormal = hit.normal;
+            float slopeAngle = Vector3.Angle(hit.normal, Vector3.up);
+            if(slopeAngle < maxSlopeAngle && slopeAngle > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
