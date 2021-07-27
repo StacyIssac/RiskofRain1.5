3f6a70ca8200db4e37e5f071b2f59ee9e03a89fb
StacyIssac <68945421+StacyIssac@users.noreply.github.com>
2021-07-27 21:09:09 +0800

    up

--- Assets/Scripts/MainScripts/MainSceneController.cs
+++ Assets/Scripts/MainScripts/MainSceneController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainSceneController : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class MainSceneController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void StartGame()
+    {
+
     }
 }
